623652448f5682640e81702ab12989536d46b52f
AldousDR <aldousjohn.delosreyes@gmail.com>
2023-04-24 11:28:13 +0800

    added Activity 3

---
+++ FY23.28 Java BED Activity 3/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+    Employee employee1 = new Employee();
+    employee1.printDetails();
+
+    System.out.println();
+
+    Employee employee2 = new Employee(2, "Aldous", "Delos Reyes", "ATCP", "ASE");
+    employee2.printDetails();
+
+    }
+
+}
